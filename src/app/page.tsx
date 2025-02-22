ea79574f35905de22efc3d2c9f23f62dbc34daa5
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2025-02-22 11:39:32 +0900

    loginしていたらpostsに飛ぶように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/app/_hooks/useAuth";
 
 // WelcomePageコンポーネント
 const Page: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [userName, setUserName] = useState<string>("");
+  const { isLoading, session } = useAuth();
 
   const router = useRouter();
 
@@ -14,24 +16,42 @@ const Page: React.FC = () => {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1 className="">実際に機能を体験しながらUnityを学ぼう!!</h1>
       <h1 className="py-1 text-5xl">Uinty-Study-Notes</h1>
-      {isLoggedIn ? (
-        <h2 className="pt-5">{userName}さん、ようこそ！</h2>
-      ) : (
-        <h2 className="pt-5">
-          現在、ログインしていません。ログインしてください。
-        </h2>
-      )}
-      <div className="pt-3">
-        <button
-          onClick={() => router.replace("/login")}
-          className={twMerge(
-            "rounded-md px-5 py-2 font-bold",
-            "bg-black text-white hover:bg-slate-700"
-          )}
-        >
-          ログイン
-        </button>
-      </div>
+      {!isLoading &&
+        (session ? (
+          <div>
+            <h2 className="pt-5">
+              ログインしています。Unity-Studyにようこそ！
+            </h2>
+            <div className="pt-3">
+              <button
+                onClick={() => router.replace("/posts")}
+                className={twMerge(
+                  "rounded-md px-5 py-2 font-bold",
+                  "bg-black text-white hover:bg-slate-700"
+                )}
+              >
+                Posts
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h2 className="pt-5">
+              現在、ログインしていません。ログインしてください。
+            </h2>
+            <div className="pt-3">
+              <button
+                onClick={() => router.replace("/login")}
+                className={twMerge(
+                  "rounded-md px-5 py-2 font-bold",
+                  "bg-black text-white hover:bg-slate-700"
+                )}
+              >
+                ログイン
+              </button>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
